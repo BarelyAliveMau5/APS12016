fbef575cc10677d9616922d6ffc01dfc698c4880
BarelyAliveMau5 <theuser@user-PC.this.org>
2016-05-28 22:35:30 -0300

    thread separada

--- Algordenamento/classes/Bubble.java
+++ Algordenamento/classes/Bubble.java
@@ -1,23 +1,35 @@
 package classes;
 
-public class Bubble
+public class Bubble extends BaseT implements Runnable
 {
-    public String[] bubble_srt(String values[])
+    public Bubble(String[] nomes)
     {
-        int n = values.length;
+        this.nomes = nomes;
+        processado = 0;
+    }
+    
+    public void bubble_srt()
+    {
+        int n = nomes.length;
         String temp;
         for (int j = n; j >= 0; j--)
         {
             for (int i = 0; i < n - 1; i++)
             {
-                if (values[i].compareTo(values[j]) < 0)
+                if (nomes[i].compareTo(nomes[j]) < 0)
                 {
-                    temp = values[i];
-                    values[i] = values[j];
-                    values[j] = temp;
+                    temp = nomes[i];
+                    nomes[i] = nomes[j];
+                    nomes[j] = temp;
                 }
             }
+            processado++;
         }
-        return values;
+    }
+
+    @Override
+    public void run()
+    {
+        bubble_srt();
     }
 }
