0ddaaea222112aa0db103d3f4489131cd74512ab
BarelyAliveMau5 <theuser@user-PC.this.org>
2016-05-29 01:12:05 -0300

    quickfix: variavel concluido

--- Algordenamento/classes/BaseT.java
+++ Algordenamento/classes/BaseT.java
@@ -2,8 +2,13 @@ package classes;
 
 public abstract class BaseT
 {
+    //o array que guarda os nomes, por algum motivo tem memory leak
     protected String nomes[];
+
+    // operações realizadas. ao invez de porcentagem, que é mais lento
     protected int processado;
+    
+    protected boolean concluido;
     
     /**
      * getter do processado
